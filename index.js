df718f40110ba758d12930dc90a25a72f95cd93d
Lexsa Campbell <lexsacampbell@lexsas-mbp.lan>
2023-02-01 22:04:15 -0800

    Updated links through primary and secondary navs

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
-// when someone clicks the hamburger button
-// if the nav is closed, open it
-// if the nav is open, close it
+// Mobile nav logic
 
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
+// when the hamburger button is clicked 
 navToggle.addEventListener("click", () => {
     const visiblity = nav.getAttribute("data-visible");
 
+    // if the nav is closed, open it 
     if (visiblity === "false") {
         nav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", true);
-    } else {
+    } 
+    // if the nav is open, close it
+    else {
         nav.setAttribute("data-visible", false);
         navToggle.setAttribute("aria-expanded", false);
     }
 })
+
+// Planet tabs logic
+
+const tabList = document.querySelector(".tab-list");
+const tabs = tabList.querySelectorAll(".tab-link");
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel)
+});
+
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    
+    const tabContainer = targetTab.parentNode;
+    
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
+        
+    targetTab.setAttribute("aria-selected", true);
+    
+    mainContainer
+        .querySelectorAll('[role="tabpanel"]')
+        .forEach((panel) => panel.setAttribute("hidden", true));
+    
+    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    
+    mainContainer
+        .querySelectorAll('picture')
+        .forEach((picture) => picture.setAttribute("hidden", true));
+        
+    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
+
+}
